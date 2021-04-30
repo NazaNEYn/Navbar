a6b875b26f6cabfaa0f5f21fe9106662dd117613
Nazanin Ashrafi <68081983+NazaNEYn@users.noreply.github.com>
2021-04-30 18:52:24 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// function openSlideMenu() {
+//     document.getElementById("menu").style.width = "100%";
+//     document.getElementById("content").style.marginLeft = "250px";
+// }
+
+// function closeSlideMenu() {
+//     document.getElementById("menu").style.width = "0";
+//     document.getElementById("content").style.marginLeft = "0";
+// }
+
+
+// function openMenu() {
+//     document.getElementById("menu").style.width = "200px"
+//     document.getElementById("content").style.marginLeft = "200px"
+
+// }
+
+// function closeMenu() {
+//     document.getElementById("menu").style.width = "0"
+//     document.getElementById("content").style.marginLeft = "0"
+
+// }
+
+function openMenu() {
+    document.getElementById("menu").style.minHeight = "100%";
+    // document.getElementById("content").style.marginTop = "100%";
+}
+
+function closeMenu() {
+    document.getElementById("menu").style.minHeight = "0";
+    // document.getElementById("content").style.marginTop = "0";
+}
